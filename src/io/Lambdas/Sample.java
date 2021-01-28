eeeb8ea1b2eb0b71fe2435fe67f7e19ea99d1b36
Priyobrato Das <priyodas12@gmail.com>
2021-01-28 14:30:41 +0530

    1

---
+++ src/io/Lambdas/Sample.java
@@ -0,0 +1,15 @@
+package io.Lambdas;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Sample {
+    public static void main(String[] args) {
+        List<Integer> numbers= Arrays.asList(23,32,34,45,65,53,89);
+
+        numbers.stream()
+               // .map(num->Integer.toString(num))
+                .map(String::valueOf)
+                .forEach(System.out::println);
+    }
+}
