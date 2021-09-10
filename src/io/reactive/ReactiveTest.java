4e4590c29a2caf549a851d257f21cefcf22b44f5
Priyobrato Das <priyodas12@gmail.com>
2021-09-10 20:22:19 +0530

    Java-091021:Reactive Programming stream.

--- src/io/reactive/ReactiveTest.java
+++ src/io/reactive/ReactiveTest.java
@@ -1,4 +1,17 @@
 package io.reactive;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+//functional: declarative + higher order functions.
 public class ReactiveTest {
+    private static boolean isEven(int i){
+        System.out.println(i);
+        return i%2==0;
+    }
+    public static void main(String[] args) {
+        var numbers= Arrays.asList(1,2,3,4,5,6);
+        var doubledNumbers=numbers.parallelStream().filter(ReactiveTest::isEven).map(n->n*2).collect(Collectors.toList());
+        System.out.println(doubledNumbers);
+    }
 }
+
