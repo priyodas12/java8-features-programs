0d93f89bb87751cd1583b0ac099f8aaa7fd35f72
Priyobrato Das <priyodas12@gmail.com>
2021-09-10 20:56:55 +0530

    Java-091021:Reactive Programming stream.

--- src/io/reactive/ReactiveTest.java
+++ src/io/reactive/ReactiveTest.java
@@ -3,6 +3,7 @@ package io.reactive;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 //functional: declarative + higher order functions.
+//reactive: elastic + message-driven + responsive + resilient
 public class ReactiveTest {
     private static boolean isEven(int i){
         System.out.println(i);
